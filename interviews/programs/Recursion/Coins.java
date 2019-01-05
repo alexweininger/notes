47868a25dff498a190601451e6096dda5f1d3362
alexweininger <alex.weininger@live.com>
2019-01-04 16:45:49 -0800

    coins

--- interviews/programs/Recursion/Coins.java
+++ interviews/programs/Recursion/Coins.java
@@ -1,20 +1,38 @@
 class Coins {
 
 	// returns the number of ways to represent the given integer n with an infinite amount of coins
-	public static int coins(int n) {
-		if (n == 0)
-			return 0;
-		else if (n == 1)
-			return 1;
-		else if (n == 5)
-			return 1 + coins(n - 1);
-		else if (n == 10)
-			return 1 + coins(n - 1);
-		else if (n == 25)
-			return 1 + coins(n - 1);
+	public static int coins(int n, int denom) {
+
+
+		// for each number of coins
+		// if it is larger than a coin, then calculate the ways for each number of coins
+		int next = 0;
+		switch (denom) {
+			case 25:
+				next = 10;
+				break;
+			case 10:
+				next = 5;
+				break;
+			case 5:
+				next = 1;
+				break;
+			case 1:
+				return 1;
+		}
+		int ways = 0;
+		for (int i = 0; i * denom <= n; i++) {
+			ways += coins(n - (i * denom), next);
+		}
+
+		return ways;
 	}
 
 	public static void main(String[] args) {
-
+		System.out.println(coins(10, 25));
+		System.out.println(coins(1, 25));
+		System.out.println(coins(2, 25));
+		System.out.println(coins(5, 25));
+		System.out.println("hello");
 	}
 }
